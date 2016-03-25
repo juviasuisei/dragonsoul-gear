d0b7a06a4832947c77c2c129a98c83e82b62378b
J. A. Mercury <j-a-mercury@users.noreply.github.com>
2016-03-25 00:25:01 -0400

    basic node.js setup

---
+++ index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static(__dirname + '*.json'));
+
+app.get('*', function(req, res) {
+	res.sendFile(__dirname + '/index.html');
+});
+
+var portaroo = process.env.PORT || 3000;
+
+var server = app.listen(portaroo, function() {
+	var host = server.address().address;
+	var port = server.address().port;
+
+	console.log('App listening at http://%s:%s', host, port);
+});
