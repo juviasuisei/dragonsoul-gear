de66e653464aad55f645b67d187d8a248f2a5063
J. A. Mercury <j-a-mercury@users.noreply.github.com>
2016-03-25 01:11:04 -0400

    perhaps the fix?

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static(__dirname + '*.json'));
+app.use(express.static(__dirname + '/json'));
 
 app.get('*', function(req, res) {
 	res.sendFile(__dirname + '/index.html');
