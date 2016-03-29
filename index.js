675c07e19d4d401497b0136357cb3e60f1bc48ff
J. A. Mercury <j-a-mercury@users.noreply.github.com>
2016-03-28 21:15:06 -0400

    v0.02 yolo; cookie storing and reset

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.use(express.static(__dirname + '/json'));
-app.use(express.static(__dirname + '/node-modules/js-cookie/src'));
+app.use(express.static(__dirname + '/node_modules/js-cookie/src'));
 
 app.get('*', function(req, res) {
 	res.sendFile(__dirname + '/index.html');
