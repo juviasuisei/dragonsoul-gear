74cb113ee21e98666d8d5f8d414abf06899b738d
J. A. Mercury <j-a-mercury@users.noreply.github.com>
2016-05-22 16:40:23 -0400

    extraneou`

--- js/app.js
+++ js/app.js
@@ -349,7 +349,7 @@ function calculate_stats(hk) {
 	console.log(hk);
 	console.log(stars);
 	console.log(base_stats);
-	base_stats.strength_growth += ((stars - 1) * heroes[hk].stat_growth.strength) * base_stats.strength_growth;`
+	base_stats.strength_growth += ((stars - 1) * heroes[hk].stat_growth.strength) * base_stats.strength_growth;
 	base_stats.agility_growth += ((stars - 1) * heroes[hk].stat_growth.agility) * base_stats.agility_growth;
 	base_stats.intellect_growth += ((stars - 1) * heroes[hk].stat_growth.intellect) * base_stats.intellect_growth;
 	console.log(base_stats);
