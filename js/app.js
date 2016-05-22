f24645fa5f21b3b621d6a480765d116723a5b0e5
J. A. Mercury <j-a-mercury@users.noreply.github.com>
2016-05-22 17:10:05 -0400

    we be debugging

--- js/app.js
+++ js/app.js
@@ -346,14 +346,15 @@ function calculate_stats(hk) {
 	var progress = $.parseJSON(localStorage.getItem(hk));
 	var level = progress.level;
 	var stars = progress.stars;
-	console.log(heroes[hk].stats);
 	console.log(hk);
+	console.log(level);
+	console.log(heroes[hk].stats);
 	base_stats.strength_growth += ((stars - 1) * heroes[hk].stat_growth.strength) * base_stats.strength_growth;
 	base_stats.agility_growth += ((stars - 1) * heroes[hk].stat_growth.agility) * base_stats.agility_growth;
 	base_stats.intellect_growth += ((stars - 1) * heroes[hk].stat_growth.intellect) * base_stats.intellect_growth;
-	base_stats.strength += level * base_stats.strength_growth;
-	base_stats.agility += level * base_stats.agility_growth;
-	base_stats.intellect += level * base_stats.intellect_growth;
+//	base_stats.strength += level * base_stats.strength_growth;
+//	base_stats.agility += level * base_stats.agility_growth;
+//	base_stats.intellect += level * base_stats.intellect_growth;
 console.log(base_stats);
 /*
 	var promotions = 0;
