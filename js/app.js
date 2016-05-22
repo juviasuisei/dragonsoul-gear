7bb68f3f6241830e7e4bb2e5438415225cf67f08
J. A. Mercury <j-a-mercury@users.noreply.github.com>
2016-05-22 16:39:22 -0400

    debugging

--- js/app.js
+++ js/app.js
@@ -346,12 +346,17 @@ function calculate_stats(hk) {
 	var progress = $.parseJSON(localStorage.getItem(hk));
 	var level = progress.level;
 	var stars = progress.stars;
-	base_stats.strength_growth += ((stars - 1) * heroes[hk].stat_growth.strength) * base_stats.strength_growth;
+	console.log(hk);
+	console.log(stars);
+	console.log(base_stats);
+	base_stats.strength_growth += ((stars - 1) * heroes[hk].stat_growth.strength) * base_stats.strength_growth;`
 	base_stats.agility_growth += ((stars - 1) * heroes[hk].stat_growth.agility) * base_stats.agility_growth;
 	base_stats.intellect_growth += ((stars - 1) * heroes[hk].stat_growth.intellect) * base_stats.intellect_growth;
+	console.log(base_stats);
 	base_stats.strength += level * base_stats.strength_growth;
 	base_stats.agility += level * base_stats.agility_growth;
 	base_stats.intellect += level * base_stats.intellect_growth;
+	console.log(base_stats);
 	var promotions = 0;
 	var promotion_bonus = 0;
 	$.each(colors, function(ck,color) {
@@ -376,6 +381,7 @@ function calculate_stats(hk) {
 	base_stats.strength += promotion_bonus;
 	base_stats.agility += promotion_bonus;
 	base_stats.intellect += promotion_bonus;
+	console.log(base_stats);
 	base_stats.max_health += 18 * base_stats.strength;
 	base_stats.armor += 0.15 * base_stats.strength;
 	base_stats.damage += 0.4 * base_stats.agility;
