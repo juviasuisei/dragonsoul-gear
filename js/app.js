ad08a79dc4997c8ef11c9590e507d4b597b45bc8
J. A. Mercury <j-a-mercury@users.noreply.github.com>
2016-05-22 17:20:43 -0400

    I hope you like syntax erroring too

--- js/app.js
+++ js/app.js
@@ -352,6 +352,9 @@ function calculate_stats(hk) {
 	base_stats.strength_growth += ((stars - 1) * heroes[hk].stat_growth.strength) * base_stats.strength_growth;
 	base_stats.agility_growth += ((stars - 1) * heroes[hk].stat_growth.agility) * base_stats.agility_growth;
 	base_stats.intellect_growth += ((stars - 1) * heroes[hk].stat_growth.intellect) * base_stats.intellect_growth;
+	base_stats.strength += level * base_stats.strength_growth;
+	base_stats.agility += level * base_stats.agility_growth;
+	base_stats.intellect += level * base_stats.intellect_growth;
 	var promotions = 0;
 	var promotion_bonus = 0;
 	$.each(colors, function(ck,color) {
@@ -362,8 +365,8 @@ function calculate_stats(hk) {
 				var gear_item = gear[heroes[hk].gearsets[color][slot]];
 				$.each(gear_item.stats, function(stat, v) {
 					if(0 !== v) {
-						base_stats[stat] += v;
 console.log(stat,v);
+						base_stats[stat] += v;
 					}
 				});
 				i++;
